08b5852c2e749004b37cd5b105baaedb693732bd
Berwyn Tan <berwyntan@gmail.com>
2022-10-11 19:07:00 +0800

    fix: cors whitelist

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://astronomy-app-2.vercel.app/',
+    'https://astronomy-app-2-59hnef6dd-berwyntan.vercel.app/',
     'http://127.0.0.1:5173',
     'http://localhost:3500',
     'http://localhost:3000',
