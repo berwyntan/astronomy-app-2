33372e9525e248a5f997adb2e8cec0004c827bd8
Berwyn Tan <berwyntan@gmail.com>
2022-10-11 20:33:53 +0800

    cors

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // middleware
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://astronomy-app-2.vercel.app');
+    next();
+  });
 app.use(credentials);
 // app.use(cookieSession(
 //     {
@@ -37,10 +41,7 @@ app.use(credentials);
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://astronomy-app-2.vercel.app');
-    next();
-  });
+
 app.use(cookieParser());
 app.use(morgan('dev'));
 
