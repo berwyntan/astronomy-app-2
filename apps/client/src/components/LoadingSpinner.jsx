ffe58fd3b3cc7b1ae298fc0ccee23b76efd03547
Berwyn Tan <berwyntan@gmail.com>
2022-11-10 22:14:46 +0800

    loading spinner fix

--- apps/client/src/components/LoadingSpinner.jsx
+++ apps/client/src/components/LoadingSpinner.jsx
@@ -1,15 +1,13 @@
-import { RevolvingDot } from 'react-loader-spinner'
+import { Circles } from 'react-loader-spinner'
 
 export default function LoadingSpinner() {
     return (
         <div className='ml-30'>
-            <RevolvingDot
-            height="100"
-            width="100"
-            radius="6"
-            color="black"
-            secondaryColor=''
-            ariaLabel="revolving-dot-loading"
+            <Circles
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="circles-loading"
             wrapperStyle={{}}
             wrapperClass=""
             visible={true}
