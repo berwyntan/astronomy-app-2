0d48765a77bcdc11449a09082e6b5e29a36a5cef
Berwyn Tan <berwyntan@gmail.com>
2023-01-07 18:06:58 +0800

    add axios to server

--- apps/server/config/allowedOrigins.js
+++ apps/server/config/allowedOrigins.js
@@ -1,7 +1,4 @@
 const allowedOrigins = [
-    'https://astronomy-app-2.vercel.app',
-    'https://astronomy-app-2-59hnef6dd-berwyntan.vercel.app',
-    'https://astronomy-app.onrender.com',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://localhost:3500',
