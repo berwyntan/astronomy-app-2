439d123e6d15012a3d5311fba926d81698fc98aa
Berwyn Tan <berwyntan@gmail.com>
2023-01-07 21:11:39 +0800

    fixed allowed origins for cors

--- apps/server/config/allowedOrigins.js
+++ apps/server/config/allowedOrigins.js
@@ -1,9 +1,7 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-    'http://localhost:3500',
     'http://localhost:3000',
-    'https://pink-delightful-hedgehog.cyclic.app'
+    'https://astronomy-app-2-client.vercel.app'
 ];
 
 module.exports = allowedOrigins;
